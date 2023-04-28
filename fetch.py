8e74cbd47b36cc69b8dff7a25c2b19bc7f2ebce8
David Dumas <david@dumas.io>
2023-04-28 13:33:14 -0500

    Initial conversion to argparse to handle cmd line args

--- fetch.py
+++ fetch.py
@@ -4,29 +4,22 @@
 import sys
 from urllib.request import urlopen
 import urllib.error
+import argparse
 
-# fetch.py 'http://example.com' out.html
-# should access "http://example.com"
-# save the payload (HTML) to a file called "out.html"
+parser = argparse.ArgumentParser()
+parser.add_argument("url", type=str, help="URL of resource to download")
+parser.add_argument("outfn", type=str, help="Output filename")
 
-# Expect sys.argv to have length 3: script name, URL, output filename
-if len(sys.argv) not in [2, 3]:
-    print("Usage: {} URL OUTFILENAME".format(sys.argv[0]))
-    exit(1)
-elif len(sys.argv) == 2:
-    # no output filename was specified; use a default
-    print("Warning: Using default output filename 'out.html'")
-    print("(This may not match the type of payload in the HTTP request.)")
-    outfn = "out.html"
-else:
-    outfn = sys.argv[2]
-
-url = sys.argv[1]
+# interpret what's in sys.argv in light of the expected
+# stuff described above, and either return an object
+# containing the arguments we declared OR notice an
+# an error and show a helpful message
+args = parser.parse_args()
 
 try:
-    with urlopen(url) as r:
-        with open(outfn, "wb") as fp:
+    with urlopen(args.url) as r:
+        with open(args.outfn, "wb") as fp:
             fp.write(r.read())
-    print("SUCCESS: Saved '{}' to '{}'".format(url, outfn))
+    print("SUCCESS: Saved '{}' to '{}'".format(args.url, args.outfn))
 except urllib.error.URLError:
-    print("ERROR: Failed to retrieve '{}'".format(url))
+    print("ERROR: Failed to retrieve '{}'".format(args.url))
