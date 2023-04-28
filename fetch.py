80ce4e1e53f5783a85f616176fa975b7a27d3033
David Dumas <david@dumas.io>
2023-04-28 10:29:13 -0500

    Use a default output filename if none on cmd line

--- fetch.py
+++ fetch.py
@@ -10,17 +10,23 @@ import urllib.error
 # save the payload (HTML) to a file called "out.html"
 
 # Expect sys.argv to have length 3: script name, URL, output filename
-if len(sys.argv) != 3:
+if len(sys.argv) not in [2, 3]:
     print("Usage: {} URL OUTFILENAME".format(sys.argv[0]))
     exit(1)
+elif len(sys.argv) == 2:
+    # no output filename was specified; use a default
+    print("Warning: Using default output filename 'out.html'")
+    print("(This may not match the type of payload in the HTTP request.)")
+    outfn = "out.html"
+else:
+    outfn = sys.argv[2]
 
 url = sys.argv[1]
-outfn = sys.argv[2]
 
 try:
     with urlopen(url) as r:
-        with open(outfn,"wb") as fp:
+        with open(outfn, "wb") as fp:
             fp.write(r.read())
-    print("SUCCESS: Saved '{}' to '{}'".format(url,outfn))
+    print("SUCCESS: Saved '{}' to '{}'".format(url, outfn))
 except urllib.error.URLError:
     print("ERROR: Failed to retrieve '{}'".format(url))
